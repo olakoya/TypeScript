15d4d9c2a84483e1da2e7cc81738658b5ec2d08f
olakoya <ola.koya1@gmail.com>
2025-10-15 23:39:22 +0100

    updates

--- Day_9/interface.js
+++ Day_9/interface.js
@@ -1,6 +1,26 @@
-// INTERFACE.TS
-// An interface in TypeScript is a syntactical contract that an entity should conform to. It defines the shape of an object, specifying what properties and methods it should have.
+"use strict";
+// =====================
+// 📐 INTERFACES
+// =====================
+// An interface in TypeScript is a syntactical contract that an entity should conform to. 
+// It defines the shape of an object, specifying what properties and methods it should have.
 // Interfaces are used to type-check whether an object adheres to a specific structure, ensuring that it has the required properties and methods with the correct types.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
 var human = {
     name: "Alice",
     age: 30,
@@ -25,4 +45,67 @@ Output:
 Hello, my name is Alice and I am 30 years old.
 8
 Hello, my name is Bob, I am 25 years old and my employee ID is 101.
-*/ 
+*/
+console.log("=====================================");
+var item = {
+    id: 101,
+    name: "Laptop"
+};
+var item2 = {
+    id: 200,
+    name: "Iphone"
+};
+console.log(item.id);
+console.log(item2.id);
+var user = {
+    name: "Pranav",
+    age: 28,
+    greet: function () {
+        console.log("Hi, I'm ".concat(this.name));
+    }
+};
+user.greet();
+var user2 = {
+    age: 25,
+    move: function () {
+        console.log("Moving");
+    },
+    fly: function () {
+        console.log("Flying");
+    }
+};
+user2.move();
+user2.fly();
+var Drone = /** @class */ (function () {
+    function Drone(age) {
+        this.age = age;
+    }
+    Drone.prototype.move = function () {
+        console.log("Drone is moving on ground.");
+    };
+    Drone.prototype.fly = function () {
+        console.log("Drone is flying in the sky.");
+    };
+    Drone.prototype.goByRoad = function () {
+        console.log("Go By Road");
+    };
+    return Drone;
+}());
+var Hero = /** @class */ (function (_super) {
+    __extends(Hero, _super);
+    function Hero() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Hero.prototype.fly = function () {
+        console.log("Flying");
+    };
+    Hero.prototype.move = function () {
+        console.log("Moving");
+    };
+    return Hero;
+}(Drone));
+var myDrone = new Drone(25);
+console.log(myDrone.age);
+myDrone.move();
+myDrone.fly();
+myDrone.goByRoad();
--- Day_9/interface.ts
+++ Day_9/interface.ts
@@ -1,6 +1,11 @@
-// INTERFACE.TS
-// An interface in TypeScript is a syntactical contract that an entity should conform to. It defines the shape of an object, specifying what properties and methods it should have.
+// =====================
+// 📐 INTERFACES
+// =====================
+// An interface in TypeScript is a syntactical contract that an entity should conform to. 
+// It defines the shape of an object, specifying what properties and methods it should have.
 // Interfaces are used to type-check whether an object adheres to a specific structure, ensuring that it has the required properties and methods with the correct types.
+
+
 
 // Example:
 interface Person {
@@ -53,3 +58,118 @@ Hello, my name is Alice and I am 30 years old.
 8
 Hello, my name is Bob, I am 25 years old and my employee ID is 101.
 */  
+console.log("=====================================")
+
+export {}
+
+// class will have the implementation also - The Functions have implementation
+// Although Interface is also a blueprint it doesn't have any implementation
+
+interface Product {
+    readonly id: number;
+    name: string;
+    description?: string;
+}
+
+let item: Product = {
+    id: 101,
+    name: "Laptop"
+};
+
+let item2: Product = {
+    id: 200,
+    name: "Iphone"
+}
+
+console.log(item.id)
+console.log(item2.id)
+
+interface Person {
+    name: string;
+    age: number;
+    greet(): void;
+}
+
+const user: Person = {
+    name: "Pranav",
+    age: 28,
+    greet() {
+        console.log(`Hi, I'm ${this.name}`);
+    }
+};
+
+user.greet();
+
+
+
+interface Movable {
+    move(): void;
+}
+
+interface Flyable extends Movable {
+    age: number
+    fly(): void;
+}
+
+interface Roadable {
+    goByRoad(): void;
+    move(): void;
+}
+
+interface Person {
+    name: string;
+    age: number;
+    greet(): void;
+}
+
+
+let user2: Flyable = {
+    age: 25,
+    move(): void {
+        console.log("Moving")
+    },
+
+    fly(): void {
+        console.log("Flying")
+    }
+}
+user2.move()
+user2.fly()
+
+class Drone implements Flyable,Roadable {
+    age:number
+    constructor(age:number) {
+        this.age = age;
+    }
+    move(): void {
+        console.log("Drone is moving on ground.");
+    }
+
+    fly(): void {
+        console.log("Drone is flying in the sky.");
+    }
+
+    goByRoad(): void {
+        console.log("Go By Road")
+    }
+}
+
+abstract class Hero extends Drone implements Flyable {
+
+
+    fly() {
+        console.log("Flying")
+    }
+
+    move() {
+        console.log("Moving")
+    }
+
+    abstract heroName(): string;
+}
+
+const myDrone = new Drone(25);
+console.log(myDrone.age)
+myDrone.move();
+myDrone.fly();
+myDrone.goByRoad();
