931b1618bd493a1c1fb65dbfca9d0679e559c239
Ola <ola.koya1@gmail.com>
2026-03-26 22:15:04 +0000

    update

--- Day_1/index.js
+++ Day_1/index.js
@@ -1,1 +1,14 @@
 console.log('Hello World');
+console.log('Welcome to JavaScript fundamentals.');
+
+// variables ??
+
+var firstname = 'John';
+var lastname = 'Doe';
+var age = 30;
+var isMarried = false;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(isMarried);
